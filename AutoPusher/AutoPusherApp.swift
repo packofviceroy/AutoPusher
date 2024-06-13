efd3b7c13503282e9e25c371d821b0fc046f3bf7
dumboProMac <hekutok@gmail.com>
2024-06-13 11:56:48 +0300

    Initial Commit

---
+++ AutoPusher/AutoPusherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPusherApp.swift
+//  AutoPusher
+//
+//  Created by dumbo on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoPusherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
